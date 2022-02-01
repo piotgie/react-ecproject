f5fa21531b770b166a00d7e954e3a30070b666f1
Piotr Gaik <piotgie@gmail.com>
2022-02-01 22:59:32 +0100

    routes

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage';
+import About from './AboutPage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Error from './ErrorPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+
+export {
+    Home,
+    About,
+    Products,
+    SingleProduct,
+    Error,
+    Cart,
+    Checkout,
+    PrivateRoute
+};
