e35e954ec9e718a87b58129aaee965f85e98eb5d
Piotr Gaik <piotgie@gmail.com>
2022-02-24 11:05:54 +0100

    filters func

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -21,6 +21,11 @@ const Filters = () => {
     all_products,
   } = useFilterContext()
 
+  const categories = getUniqueValues(all_products, 'category')
+  const companies = getUniqueValues(all_products, 'company')
+  const colors = getUniqueValues(all_products, 'colors')
+
+
   return (
     <Wrapper>
       <div className="content">
@@ -37,6 +42,84 @@ const Filters = () => {
             />
           </div>
           {/* {end search input} */}
+          {/* categories */}
+          <div className="form-control">
+            <h5>category</h5>
+            <div>
+              {
+                categories.map((c, index) => {
+                  return (
+                    <button
+                      key={index}
+                      onClick={updateFilters}
+                      name="category"
+                      type="button"
+                      className={`${category === c.toLowerCase() ? 'active' : null}`}
+                    >
+                      {c}
+                    </button>
+                  )
+                })
+              }
+            </div>
+          </div>
+          {/* end of categories */}
+          {/* companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select name="company" value={company} onChange={updateFilters} className="company">
+              {companies.map((c, index) => {
+                return <option key={index} value={c}>{c}</option>
+              })}
+            </select>
+          </div>
+          {/* end of companies */}
+          {/* colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === 'all') {
+                  return <button
+                    key={index}
+                    name="color"
+                    onClick={updateFilters}
+                    data-color='all'
+                    lassName={`${color === 'all' ? 'all-btn active' : 'all btn'}`}
+                  >
+                    all
+                  </button>
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: c }}
+                    className={`${color === c ? 'color-btn active' : 'color-btn'}`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
+          {/* price */}
+          <div className="form-control">
+            <h5>price</h5>
+            <p className="price">{formatPrice(price)}</p>
+            <input
+              type="range"
+              name="price"
+              onChange={updateFilters}
+              min={min_price}
+              max={max_price}
+              value={price}
+            />
+          </div>
+          {/* end of price */}
         </form>
       </div>
     </Wrapper>
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -62,6 +62,17 @@ export const FilterProvider = ({ children }) => {
   const updateFilters = (e) => {
     let name = e.target.name
     let value = e.target.value
+
+    if (name === 'category') {
+      value = e.target.textContent
+    }
+    if (name === 'color') {
+      value = e.target.dataset.color
+    }
+    if (name === 'price') {
+      value = Number(value)
+    }
+
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
   }
 
